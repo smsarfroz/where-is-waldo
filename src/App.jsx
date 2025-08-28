8f67be65e9388db0790ce3b504e9f3684b297f3b
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-08-28 06:47:41 +0000

    initiliaze

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+const VITE_BASE_URL =  import.meta.env.VITE_BASE_URL || '/api';
+console.log(VITE_BASE_URL);
+
+function App() {
+
+  return (
+    <>
+      <p>Hi!</p>
+    </>
+  )
+}
+
+export default App
