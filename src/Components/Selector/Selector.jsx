88994da0ee6e92fa8b74d6e36c2365cb7593ce82
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-09-04 08:20:34 +0000

    feat: create functionality to decide the side and how to place those components on a click.

--- src/Components/Selector/Selector.jsx
+++ src/Components/Selector/Selector.jsx
@@ -1,18 +1,18 @@
 import { SiCodesandbox } from "react-icons/si";
 import styles from './Selector.module.css';
 
-const Selector = ({style}) => {
+const Selector = ({style, style2}) => {
 
     return (
         <>
-            <SiCodesandbox style={style}/>
+            <SiCodesandbox style={style} size={20} className={styles.box}/>
             
-            {/* <div className={styles.dropdown}>
+            <div className={styles.dropdown} style={style2}>
                 <div className={styles.option}>Waldo</div>
                 <div className={styles.option}>Wilma</div>
                 <div className={styles.option}>Wizard</div>
                 <div className={styles.option}>Cancel</div>
-            </div> */}
+            </div>
 
         </>
     )
--- src/Components/SettingImage/SettingImage.jsx
+++ src/Components/SettingImage/SettingImage.jsx
@@ -2,36 +2,64 @@ import { useRef } from "react";
 import { useState } from "react";
 import styles from "./SettingImage.module.css";
 import Selector from "../Selector/Selector.jsx";
+const width = 500;
+const height = 500;
+const size = 20;
 
-function SettingImage (){
-    const mouseClickRef = useRef(null);
-    const [coordx, setCoordx] = useState(null);
-    const [coordy, setCoordy] = useState(null);
+function SettingImage() {
+  const mouseClickRef = useRef(null);
+  const [coordx, setCoordx] = useState(null);
+  const [coordy, setCoordy] = useState(null);
+  const [left, setLeft] = useState(true);
+  const [up, setUp] = useState(true);
 
-    const handleClick = (event) => {
-        if (mouseClickRef.current) {
-          const rect = mouseClickRef.current.getBoundingClientRect();
-          const x = event.clientX - rect.x;
-          const y = event.clientY - rect.y;
+  const handleClick = (event) => {
+    if (mouseClickRef.current) {
+      const rect = mouseClickRef.current.getBoundingClientRect();
+      const x = event.clientX - rect.x;
+      const y = event.clientY - rect.y;
 
-          console.log(`Click coordinates relative to element: X=${x}, Y=${y}`);
-          setCoordx(x);
-          setCoordy(y);
-          console.log('coordx, coordy: ', coordx, coordy);
-        }
-    };  
+      console.log(`Click coordinates relative to element: X=${x}, Y=${y}`);
+      setCoordx(x);
+      setCoordy(y); 
+      //   console.log('coordx, coordy: ', coordx, coordy); 
+      const rightSide = width - x;
+      const lowerSide = height - y;
 
-    const styleSelector = {
-        backgroundColor: 'red', 
-        transform: `translate(-50%, -50%) translate(${coordx}px, ${coordy}px)`,
-    };
+      if (rightSide > x) {
+        setLeft(false);
+      } else {
+        setLeft(true);
+      }
+      if (lowerSide > y) {
+        setUp(false);
+      } else {
+        setUp(true);
+      }
+    }
+  };
+    
+  const styleSelector = {
+    backgroundColor: "red",
+    transform: `translate(-50%, -50%) translate(${coordx}px, ${coordy}px)`,
+  };
+  console.log(left, up);
+  const style2 = {
+    backgroundColor: "blue",
+    transform: `
+      ${left ? `translate(-100%, 0) ` : ' '}
+      ${up ? `translate(0, -100%) ` : ' '}
+      translate(${coordx + size/2}px, ${coordy + size/2}px) 
+      ${left ? `translateX(${-size  }px) ` : ` `} 
+      ${up ? `translateY(${-size  }px) ` : ` `}
+      `,
+  };
 
-    return (
-        <div ref={mouseClickRef} onClick={handleClick} className={styles.Image}>
-            
-            <Selector style={styleSelector} />
-        </div>
-    )
-};
+  return (
+    <div ref={mouseClickRef} onClick={handleClick} className={styles.Image}>
+      <Selector style={styleSelector} style2={style2} />
+    </div>
+  );
+}
 
 export default SettingImage;
